67be82f5ad7daf9d9b0613a114125b8288b5ac5b
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2022-06-25 18:35:28 -0700

    add initial Query Types and Resolver

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,25 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    //1st is initial value. Since this field exists on a query object, nothing resolves before it. 
+    //2nd are the arguments that a client can pass
+    //3rd is context object. This is shared amongst all resolvers. Defined in server.js
+    //4th is AST of incoming query
+    pets(_, __, ctx) {
+      return ctx.models.Pet.findMany()
     }
   },
-  User: {
+  // Mutation: {
     
-  }
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+    
+  //}
 }
