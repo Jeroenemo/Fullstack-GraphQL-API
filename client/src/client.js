cd9706652b8b76a2e90a82c075702ea9d35b17d1
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2022-07-04 13:02:13 -0700

    compose new link to add delay

--- client/src/client.js
+++ client/src/client.js
@@ -1,13 +1,27 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import { HttpLink } from 'apollo-link-http' 
+import { ApolloLink } from 'apollo-link'
 import gql from 'graphql-tag'
+import { setContext } from 'apollo-link-context'
 
 /**
  * Create a new apollo client and export as default
  */
 //network interface to access gql server
-const link = new HttpLink({uri:'http://localhost:4000/'})
+const http = new HttpLink({uri:'http://localhost:4000/'})  
+const delay = setContext(
+  request => 
+    new Promise((success, fail) => {
+      setTimeout(() => { 
+        success()
+      }, 800)    
+    })
+)
+const link = ApolloLink.from([
+  delay, 
+  http
+])
 //In memory cache interface from apollo
 const cache = new InMemoryCache() 
 
